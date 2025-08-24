d0df2d7564845391ca178886deeab2302df89cb7
vitalii-22 <smulka69@gmail.com>
2025-08-24 23:36:26 +0300

    додав сторінку додавання нотатки

--- app/notes/action/create/page.tsx
+++ app/notes/action/create/page.tsx
@@ -5,13 +5,35 @@ import css from "./CreateNote.module.css";
 //   children: React.ReactNode;
 // }
 
+export async function generateMetadata({ params }: NoteDetailsProps) {
+  const { id } = await params;
+  const note = await fetchNoteById(id);
+  return {
+    title: "Create note",
+    description: "Page for writing note",
+    openGraph: {
+      title: ` ${note.title}`,
+      description: `${note.content}`,
+      url: "https://08-zustand-silk.vercel.app/",
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: "Note hub image",
+        },
+      ],
+    },
+  };
+}
+
 export default function CreateNote() {
   return (
     <main className={css.main}>
       <div className={css.container}>
         <h1 className={css.title}>Create note</h1>
 
-        {/* <NoteForm /> */}
+        <NoteForm />
       </div>
     </main>
   );
--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -1,27 +1,27 @@
+"use client";
+
 import { useId } from "react";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import type { FormikHelpers } from "formik";
 import * as Yup from "yup";
 
 import css from "./NoteForm.module.css";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import type { NewNoteData } from "../../types/note";
 import { createNote } from "@/lib/api";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 // interface NoteFormProps {
 // }
 
-const OrderFormSchema = Yup.object().shape({
-  title: Yup.string()
-    .min(3, "Title must be at least 3 characters")
-    .max(50, "Title is too long")
-    .required("Title is required"),
-  content: Yup.string().max(500, "Content is too long"),
-  tag: Yup.string()
-    .oneOf(["Todo", "Work", "Personal", "Meeting", "Shopping"])
-    .required("Tag is required"),
-});
+// const OrderFormSchema = Yup.object().shape({
+//   title: Yup.string()
+//     .min(3, "Title must be at least 3 characters")
+//     .max(50, "Title is too long")
+//     .required("Title is required"),
+//   content: Yup.string().max(500, "Content is too long"),
+//   tag: Yup.string()
+//     .oneOf(["Todo", "Work", "Personal", "Meeting", "Shopping"])
+//     .required("Tag is required"),
+// });
 
 interface OrderFormValues {
   title: string;
@@ -37,23 +37,21 @@ const initialValues: OrderFormValues = {
 
 export default function NoteForm() {
   const fieldId = useId();
-  const queryClient = useQueryClient();
-
-  const { mutate } = useMutation({
-    mutationFn: (noteData: NewNoteData) => createNote(noteData),
-    onSuccess() {
-      queryClient.invalidateQueries({
-        queryKey: ["notes"],
-      });
-    },
-  });
 
   const router = useRouter();
 
-  const handleCancel = () => router.push("/notes/filter/all");
+  // const handleCancel = () => router.push("/notes/filter/all");
+
+  const { mutate } = useMutation({
+    mutationFn: (noteData: NewNoteData) => createNote(noteData),
+    onSuccess: () => {
+      router.push("/notes/filter/All");
+    },
+  });
 
   const handleSubmit = (formData: FormData) => {
-    const values = Object.fromEntries(formData);
+    const values = Object.fromEntries(formData) as unknown as NewNoteData;
+    mutate(values);
     console.log(values);
   };
 
@@ -65,32 +63,40 @@ export default function NoteForm() {
   //   formikHelpers.resetForm();
   // };
   return (
-    <form action={handleSubmit}>
-      <label>
-        Title
-        <input type="text" name="title" />
-      </label>
+    <form className={css.form} action={handleSubmit}>
+      <div className={css.formGroup}>
+        <label htmlFor={`${fieldId}-title`}>Title</label>
+        <input
+          id={`${fieldId}-title`}
+          type="text"
+          name="title"
+          className={css.input}
+        />
+      </div>
 
-      <label>
-        Content
-        <textarea name="content"></textarea>
-      </label>
+      <div className={css.formGroup}>
+        <label htmlFor={`${fieldId}-content`}>Content</label>
+        <textarea
+          id={`${fieldId}-content`}
+          name="content"
+          className={css.textarea}
+        ></textarea>
+      </div>
 
-      <label>
-        Tag
-        <select name="tag" className={css.select}>
+      <div className={css.formGroup}>
+        <label htmlFor={`${fieldId}-tag`}>Tag</label>
+        <select id={`${fieldId}-tag`} name="tag" className={css.select}>
           <option value="Todo">Todo</option>
           <option value="Work">Work</option>
           <option value="Personal">Personal</option>
           <option value="Meeting">Meeting</option>
           <option value="Shopping">Shopping</option>
         </select>
-      </label>
+      </div>
 
-      <div>
-        <button type="submit">Create</button>
-        <button type="button" onClick={handleCancel}>
-          Cancel
+      <div className={css.actions}>
+        <button type="submit" className={css.submitButton}>
+          Create note
         </button>
       </div>
     </form>
